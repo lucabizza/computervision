159d90087a366e66a9d63014b4f7c5fe2bbebeec
lucabizzaro <luca.b@riskapp.it>
2018-11-04 20:28:31 +0100

    people_counter

---
+++ trackableobject.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Script to track an object
+Created on 04/11/2018
+
+@author: lucab
+"""
+
+
+class TrackableObject:
+    def __init__(self, objectID, centroid):
+        # store the object ID, then initialize a list of centroids
+        # using the current centroid
+        self.objectID = objectID
+        self.centroids = [centroid]
+
+        # initialize a boolean used to indicate if the object has
+        # already been counted or not
+        self.counted = False
